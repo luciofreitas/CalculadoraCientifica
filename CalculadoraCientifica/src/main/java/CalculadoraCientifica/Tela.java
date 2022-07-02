89020cbf7a3c34c305f30765784acd93bdfc2225
luciofreitas <luciodfp@gmail.com>
2022-07-02 00:53:39 -0300

    .

--- CalculadoraCientifica/src/main/java/CalculadoraCientifica/Tela.java
+++ CalculadoraCientifica/src/main/java/CalculadoraCientifica/Tela.java
@@ -48,6 +48,7 @@ public class Tela extends javax.swing.JFrame {
         jToolBar2 = new javax.swing.JToolBar();
         jPanel3 = new javax.swing.JPanel();
         jPanel4 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
         jToolBar3 = new javax.swing.JToolBar();
         apagarCompleto = new javax.swing.JButton();
         apagar = new javax.swing.JButton();
@@ -251,15 +252,26 @@ public class Tela extends javax.swing.JFrame {
 
         jToolBar2.setRollover(true);
 
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/porcentagem.png"))); // NOI18N
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
         jPanel4Layout.setHorizontalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 352, Short.MAX_VALUE)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 297, Short.MAX_VALUE))
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 270, Short.MAX_VALUE)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 235, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
@@ -432,6 +444,10 @@ public class Tela extends javax.swing.JFrame {
         tela.setText(str);
     }//GEN-LAST:event_apagarActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -475,6 +491,7 @@ public class Tela extends javax.swing.JFrame {
     private javax.swing.JButton divisão;
     private javax.swing.JButton dois;
     private javax.swing.JButton igual;
+    private javax.swing.JButton jButton1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
